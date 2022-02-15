98d2f54cf1bcb948125a2779691f13b564816e00
Justfemi <ajibadeolufemi94@gmail.com>
2022-02-15 16:42:07 +0100

    ft:added popup functionality

--- index.js
+++ index.js
@@ -5,3 +5,26 @@ menu.onclick = () => {
   menu.classList.toggle('fa-times');
   sideBar.classList.toggle('active');
 }
+
+window.onscroll = () => {
+  menu.classList.remove('fa-times');
+  sideBar.classList.remove('active');
+}
+
+let popup = document.querySelector('.popup__image');
+
+popup.onclick = () => {
+  popup.style.display = 'none';
+};
+
+let images = document.querySelectorAll('.image__container img');
+
+images.forEach (img => {
+  img.onclick = () => {
+    let imgSrc = img.getAttribute('src');
+    popup.style.display = 'flex';
+    popup.querySelector('img').src = imgSrc
+  }
+});
+
+
